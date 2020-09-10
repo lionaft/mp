77f4bdee893a4316de6745838e0f0eeceaf254f1
lionaft <lotar2k@yandex.ru>
2020-09-10 15:44:53 +0300

    first commit

---
+++ src/java/ru/sberbank/ManagementPoint/ManagementPointReceive.java
@@ -0,0 +1,27 @@
+package ru.sberbank.ManagementPoint;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/receive")
+public class ManagementPointReceive{
+
+    @POST
+    @Path("/data")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response ReceiveData(String jsonData) throws JsonParseException {
+
+        JsonObject reseiveObject = new JsonObject();
+        JsonObject responseObject = new JsonObject();
+
+        reseiveObject = reseiveObject.getAsJsonObject(jsonData);
+        responseObject.addProperty("Success", true);
+
+        return Response.status(200).entity(responseObject).build();
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>This is page for test work capability of Management Point. If you see this - MP is working!</h2>
+</body>
+</html>
