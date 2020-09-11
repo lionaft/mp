7e43c00baf54668b9e6921071368992aec27058c
lionaft <lotar2k@yandex.ru>
2020-09-11 15:20:54 +0300

    add check Kafka properties

--- src/main/java/ru/sberbank/ManagementPoint/GenerateProducer.java
+++ src/main/java/ru/sberbank/ManagementPoint/GenerateProducer.java
@@ -31,8 +31,8 @@ public class GenerateProducer {
         } catch (IOException ex) {
             MainLogger.error("MP", ex);
         }
-
-        prop.put("client.id", InetAddress.getLocalHost().getHostName());
+        MainLogger.info("MP",prop.toString());
+        prop.setProperty("client.id", InetAddress.getLocalHost().getHostName());
         MainLogger.info("MP",prop.toString());
 
         producer = new KafkaProducer<>(prop);
