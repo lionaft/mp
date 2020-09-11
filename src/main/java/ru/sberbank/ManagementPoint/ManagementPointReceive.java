bc53aaee32f7275f97416839acd29476c806fcba
lionaft <lotar2k@yandex.ru>
2020-09-11 14:15:29 +0300

    add message logging

--- src/main/java/ru/sberbank/ManagementPoint/ManagementPointReceive.java
+++ src/main/java/ru/sberbank/ManagementPoint/ManagementPointReceive.java
@@ -2,15 +2,17 @@ package ru.sberbank.ManagementPoint;
 
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParseException;
-import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.clients.producer.RecordMetadata;
 import ru.sberbank.meta.logging.MainLogger;
 
-import javax.ws.rs.*;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.net.UnknownHostException;
 
 @Path("/receive")
 public class ManagementPointReceive{
@@ -19,13 +21,16 @@ public class ManagementPointReceive{
     @Path("/data")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public Response ReceiveData(String jsonData) throws JsonParseException {
+    public Response ReceiveData(String jsonData) throws JsonParseException, UnknownHostException {
 
         JsonObject reseiveObject = new JsonObject();
         JsonObject responseObject = new JsonObject();
 
         reseiveObject = reseiveObject.getAsJsonObject(jsonData);
         responseObject.addProperty("Success", true);
+        GenerateProducer.getProducer();
+        MainLogger.info("MP", jsonData);
+        MainLogger.info("MP", reseiveObject.toString());
         SendToKafka(reseiveObject);
 
         return Response.status(200).entity(responseObject.toString()).build();
@@ -35,12 +40,10 @@ public class ManagementPointReceive{
         final ProducerRecord record = new ProducerRecord("DataWriterQueue", json.get("type"), json.toString());
         try {
             Producer prod = GenerateProducer.getProducer();
-            GenerateProducer.getProducer().send(record, new Callback() {
-                public void onCompletion(RecordMetadata metadata, Exception e) {
-                    if (e != null)
-                        MainLogger.info("MP", String.format("Send failed for record %s : %s", record.toString(), e.toString()));
-                        MainLogger.error("MP", e);
-                }
+            prod.send(record, (metadata, e) -> {
+                if (e != null)
+                    MainLogger.info("MP", String.format("Send failed for record %s : %s", record.toString(), e.toString()));
+                    MainLogger.error("MP", e);
             });
         }
         catch (Exception ex){
